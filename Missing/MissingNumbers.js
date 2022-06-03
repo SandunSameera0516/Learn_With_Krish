a65361838e4160f96c992c39870bd0f82cf34dce
Sandun Sameera de Silva <59699372+SandunSameera0516@users.noreply.github.com>
2022-06-03 23:07:42 +0530

    Update MissingNumbers.js

--- Missing/MissingNumbers.js
+++ Missing/MissingNumbers.js
@@ -1,6 +1,6 @@
 
 
-const a  = [21,25,29,28,22,24,27,26,30];
+/* const a  = [21,25,29,28,22,24,27,26,30];
 	a.sort()
 
 	for(var i=0; i<a.length-1;i++){
@@ -15,5 +15,16 @@ const a  = [21,25,29,28,22,24,27,26,30];
 			console.log(c+1);
 		}
 	}
+*/
+let a  = [21,25,29,28,22,24,27,26,30]; 
+a.sort()
+maxval = 30;
+missing = []
 
+for(let i = 21; i <= maxval; i++){
+	if(a.indexOf(i) === -1){
+		missing.push(i)
+	}
+}
+console.log(missing)
 	
