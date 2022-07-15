ffb5f504293986523923e2be458ee7f9b1f4368c
Sandun Sameera de Silva <59699372+SandunSameera0516@users.noreply.github.com>
2022-07-15 19:56:59 +0530

    Memento Design Pattern

---
+++ Memento Design Pattern/Design.java
@@ -0,0 +1,26 @@
+package com.sandun;
+import java.util.List;
+import java.util.ArrayList;
+public class Design {
+
+    public static void main(String[] args)
+    {
+
+        List<Life.Memento> savedTimes = new ArrayList<Life.Memento>();
+
+        Life life = new Life();
+
+        //time travel and record the eras
+        life.set("1000 B.C.");
+        savedTimes.add(life.saveToMemento());
+        life.set("1000 A.D.");
+        savedTimes.add(life.saveToMemento());
+        life.set("2000 A.D.");
+        savedTimes.add(life.saveToMemento());
+        life.set("4000 A.D.");
+
+        life.restoreFromMemento(savedTimes.get(0));
+
+    }
+
+}
