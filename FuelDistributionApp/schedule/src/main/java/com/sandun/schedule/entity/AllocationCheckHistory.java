49b701f34be046e4acb4a1ebf88dc876f217ffda
Sandun Sameera <sanduns160@gmail.com>
2022-09-07 12:31:45 +0530

    Updated

--- FuelDistributionApp/schedule/src/main/java/com/sandun/schedule/entity/AllocationCheckHistory.java
+++ FuelDistributionApp/schedule/src/main/java/com/sandun/schedule/entity/AllocationCheckHistory.java
@@ -5,11 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.Id;
 import java.util.ArrayList;
 
-//Allocation Class where we define our variables of the class
+// Allocation Class where we define our variables of the class
 @Data
 @Builder
 @AllArgsConstructor
@@ -18,7 +17,7 @@ public class AllocationCheckHistory {
     @Id
     private Long id;
     private Integer orderId;
-    private Integer allocAmount;
+    private Integer allocAmount; 
     private String status;
     private Boolean isStockAvailable;
     private ArrayList<Integer> createdAt;
